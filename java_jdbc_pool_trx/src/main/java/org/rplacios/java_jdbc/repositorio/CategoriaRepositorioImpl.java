2ee3e1307706d5d46470fef90c52ff61cd6e0646
Rene Rodriguez <rodriguezreneisrael@gmail.com>
2026-02-22 10:29:09 -0600

    JDBC-POOL

---
+++ java_jdbc_pool_trx/src/main/java/org/rplacios/java_jdbc/repositorio/CategoriaRepositorioImpl.java
@@ -0,0 +1,92 @@
+package org.rplacios.java_jdbc.repositorio;
+
+import org.rplacios.java_jdbc.modelo.Categoria;
+import org.rplacios.java_jdbc.modelo.Producto;
+import org.rplacios.java_jdbc.util.ConexionBaseDatos;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaRepositorioImpl implements Repositorio<Categoria>{
+    private Connection conn;
+
+    public CategoriaRepositorioImpl(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public List<Categoria> listar() {
+        List<Categoria> c = new ArrayList<>();
+        try (Statement s = conn.createStatement()){
+            ResultSet r = s.executeQuery("SELECT * FROM categoria");
+            while (r.next()){
+                Categoria ct = crearCategoria(r);
+                c.add(ct);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return c;
+    }
+
+    @Override
+    public Categoria porId(long id) {
+        Categoria categoria = null;
+        try(PreparedStatement st = conn.prepareStatement("SELECT * FROM categoria WHERE idCategoria = ?")){
+            st.setLong(1,id);
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    categoria = crearCategoria(rs);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return categoria;
+    }
+
+    @Override
+        public Categoria save(Categoria categoria) {
+        String sql = null;
+        if (categoria.getId() != null && categoria.getId()>0) {
+            sql = "UPDATE categoria SET nombreCategoria=? WHERE idCategoria=?";
+        } else {
+            sql = "INSERT INTO categoria(nombreCategoria) VALUES(?)";
+        }
+        try(PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            st.setString(1, categoria.getNombre());
+
+            st.executeUpdate();
+            if (categoria.getId() == null){
+                try(ResultSet rs = st.getGeneratedKeys()){
+                    if (rs.next()){
+                        categoria.setId(rs.getLong(1));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categoria;
+
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        try(PreparedStatement st = conn.prepareStatement("DELETE FROM categoria WHERE idCategoria=?")){
+            st.setLong(1,id);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Categoria crearCategoria(ResultSet r) throws SQLException {
+        Categoria c = new Categoria();
+        c.setId(r.getLong("idCategoria"));
+        c.setNombre(r.getString("nombreCategoria"));
+        return c;
+    }
+}
